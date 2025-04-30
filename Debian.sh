9776bfbf6dc5e87d45b5137266c8cfbbffe4ccfc
Konata09 <lz9709@gmail.com>
2025-04-30 17:17:40 +0700

    update for debian 13

--- Debian.sh
+++ Debian.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-apt update && apt install -y unzip htop vim git curl wget net-tools ca-certificates openssl zsh libncursesw5-dev autotools-dev autoconf build-essential iperf3 neofetch grc gnupg2 lsb-release debian-archive-keyring socat
+apt update && apt install -y unzip htop vim git curl wget net-tools ca-certificates openssl zsh libncursesw5-dev autotools-dev autoconf build-essential iperf3 fastfetch grc gnupg2 lsb-release debian-archive-keyring socat
 
 # add ssh keys
 mkdir -p ~/.ssh && chmod 700 ~/.ssh
@@ -23,9 +23,19 @@ wget https://install.speedtest.net/app/cli/ookla-speedtest-1.0.0-x86_64-linux.tg
 tar -xvf ookla-speedtest-1.0.0-x86_64-linux.tgz
 rm ookla-speedtest-1.0.0-x86_64-linux.tgz
 
+# copy fastfetch configuration
+mkdir -p /root/.config/fastfetch
+curl -fSL https://raw.githubusercontent.com/Konata09/VPSScript/master/fastfetch.jsonc > /root/.config/neofetch/config.jsonc
+
 # copy neofetch configuration
-mkdir -p /root/.config/neofetch
-curl -fSL https://raw.githubusercontent.com/Konata09/VPSScript/master/neo.conf > /root/.config/neofetch/config.conf
+# mkdir -p /root/.config/neofetch
+# curl -fSL https://raw.githubusercontent.com/Konata09/VPSScript/master/neo.conf > /root/.config/neofetch/config.conf
+
+# install xanmod kernel
+wget -qO - https://dl.xanmod.org/archive.key | gpg --dearmor -vo /etc/apt/keyrings/xanmod-archive-keyring.gpg
+echo 'deb [signed-by=/etc/apt/keyrings/xanmod-archive-keyring.gpg] http://deb.xanmod.org releases main' | tee /etc/apt/sources.list.d/xanmod-release.list
+apt update && apt install linux-xanmod-x64v3
+update-grub
 
 # install nginx
 curl https://nginx.org/keys/nginx_signing.key | gpg --dearmor | sudo tee /usr/share/keyrings/nginx-archive-keyring.gpg >/dev/null
